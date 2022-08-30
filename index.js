76fdf527e8e8f92c222ca125e51f388e7a1d79b4
David Romario <romario.david1997@gmail.com>
2022-08-30 19:06:39 -0300

    funcionalidade do botao adicionar do carrinho

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function principal() {
+    var produtoPronto = false;
+
     function listagemDeProdutos(home) {
         fetch('./dados/produtos.json')
             .then(resposta => resposta.json())
@@ -29,28 +31,28 @@ function principal() {
     }
 
     function pegarProduto() {
-         const queryString = window.location.search; // pega o texto da url
-         // método split que utiliza o separador = para retornar o que tem depois dele
-         const queryArray =  queryString.split("="); // retorna um array onde produto é o indice 0 e o ID indice 1
-         const idProduto = queryArray[1]; // acessa o ID pelo indice 1
+        const queryString = window.location.search; // pega o texto da url
+        // método split que utiliza o separador = para retornar o que tem depois dele
+        const queryArray = queryString.split("="); // retorna um array onde produto é o indice 0 e o ID indice 1
+        const idProduto = queryArray[1]; // acessa o ID pelo indice 1
         fetch('./dados/produtos.json')
             .then((resposta) => resposta.json())
             .then((dados) => {
                 dados.map((produto) => {
-                     const main = document.querySelector("main");
-                     if (main !== null) {
-                        if (idProduto != 'undefined' && parseInt(idProduto) == produto.id)//verifica se tem produto na url e transforma string em numero 
+                    const main = document.querySelector("main");
+                    if (main !== null) {
+                        if (idProduto != 'undefined' && parseInt(idProduto) == produto.id) //verifica se tem produto na url e transforma string em numero 
                         {
-                           main.innerHTML = montaHTMLproduto(produto.nome, produto.valor, produto.imagem, produto.descricao, produto.id);
-                        } 
-                     }
-                     const carrinhoProduto = document.getElementById("produto-carrinho");
-                     if (carrinhoProduto !== null) {
-                         if (idProduto != 'undefined' && parseInt(idProduto) == produto.id) {
-                             carrinhoProduto.innerHTML = montaHTMLcarrinho(produto.nome, produto.imagem, produto.valor, produto.id);
-                             
-                         }
-                     }
+                            main.innerHTML = montaHTMLproduto(produto.nome, produto.valor, produto.imagem, produto.descricao, produto.id);
+                        }
+                    }
+                    const carrinhoProduto = document.getElementById("produto-carrinho");
+                    if (carrinhoProduto !== null) {
+                        if (idProduto != 'undefined' && parseInt(idProduto) == produto.id) {
+                            carrinhoProduto.innerHTML = montaHTMLcarrinho(produto.nome, produto.imagem, produto.valor, produto.id);
+                            produtoPronto = true;
+                        }
+                    }
 
 
                 });
@@ -72,25 +74,52 @@ function principal() {
       </div>
     </div> `
     }
+    
+    function montaHTMLcarrinho(nome, imagem, preco, id) {
+        return `<div class="product">
+          <img class="photo" src="${imagem}" alt="" />
+          <h1 id="produto-nome">${nome}</h1>
+          <p id="produto-preco">${preco}</p>
+          <div class="button">
+          <button id = "bt1"> - </button>
+            <input id="campo" type="text" value="1">
+            <button id="bt2"> + </button>
+          </div>
+        </div>
+        <div class="button2">
+          <button class="payment">Pagamento</button>
+        </div>`
+    
+      }
+      // setTimeout(() => {
+      //   var botao = document.getElementById('bt1');
+      //   var valor = 0
+
+      //   if (botao !== null) {
+      //     var campo = document.getElementById('campo');
+
+      //     botao.addEventListener('click', function (e) {
+      //       campo.value = valor--
+      //     });
+
+      //   }
+      // }, 2000);
+
+      //settimeout atrasar a funcionalidade do botao
+      setTimeout(() => {
+        var botao = document.getElementById('bt2');
+        var valor = 2;
+        
+         if (botao !== null) {
+            var campo = document.getElementById('campo');
+
+           botao.addEventListener('click', function (e) {
+             campo.value = valor++
+           });
+
+         }
+      }, 1000);
+      
 }
-
-
-function montaHTMLcarrinho(nome, imagem, preco, id) {
-   return `<div class="product">
-      <img class="photo" src="${imagem}" alt="" />
-      <h1 id="produto-nome">${nome}</h1>
-      <p id="produto-preco">${preco}</p>
-      <div class="button">
-        <input type="text" value="1" />
-        <button>+</button>
-      </div>
-    </div>
-    <div class="button2">
-      <button class="payment">Pagamento</button>
-    </div>`
-
-}
-
-
 
 principal();
